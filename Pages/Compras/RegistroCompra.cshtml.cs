7e13ce1ca42de542c2a146661934ed328363beac
vernesto2 <vacevedo242@gmail.com>
2019-08-31 19:28:32 -0600

    Modifique el layout e inicie el modulo de compras

--- Pages/Compras/RegistroCompra.cshtml.cs
+++ Pages/Compras/RegistroCompra.cshtml.cs
@@ -1,14 +1,33 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SGLibreria.Models;
 
-namespace SGLibreria.Pages.Compras{
-    public class RegistroCompraModel : PageModel
-    {
+namespace SGLibreria.Pages.Compras {
+    public class RegistroCompraModel : PageModel {
+        private readonly AppDbContext _context;
+
+        public RegistroCompraModel (AppDbContext context) {
+            _context = context;
+        }
+
         [BindProperty]
         public Compra Compra { get; set; }
-        public RegistroCompraModel()
-        {
+
+        [BindProperty]
+
+        public Proveedor Proveedor { get; set; }
+
+        public async Task<IActionResult> OnPostAsync () {
+            if (!ModelState.IsValid) {
+                return Page ();
+            }
+            if (!Proveedor.Enlace.Contains ("http")) {
+                Proveedor.Enlace = "http://" + Proveedor.Enlace;
+            }
+            _context.Proveedores.Add (Proveedor);
+            await _context.SaveChangesAsync ();
+            return RedirectToPage ("/Proveedores/RegistroProveedor");
         }
     }
 }
