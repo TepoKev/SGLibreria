f9604fe06c6cebf0fe9798dda31f7be749937b74
Diego Palacios <palaciosdiego759@gmail.com>
2019-09-08 06:50:32 +0200

    Algunos archivos nuevos cshtml.cs

---
+++ Pages/Categorias/RegistroCategoria.cshtml.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SGLibreria.Pages.Categorias
+{
+    public class RegistroCategoriaModel: PageModel
+    {
+        public RegistroCategoriaModel() {
+
+        }
+        public void OnGet() {
+
+        }
+        public void OnPost() {
+            
+        }
+    }
+}
