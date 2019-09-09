7a27c9c6197b53b31d25074c4dbc8d1f0ead61fa
vernesto2 <vacevedo242@gmail.com>
2019-09-08 22:17:04 -0600

    layout modificado y el modulo de compras tiene errores

--- Pages/Compras/RegistroCompra.cshtml.cs
+++ Pages/Compras/RegistroCompra.cshtml.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -6,9 +7,50 @@ using SGLibreria.Models;
 namespace SGLibreria.Pages.Compras {
     public class RegistroCompraModel : PageModel {
         private readonly AppDbContext _context;
-
+        public List<Producto> Productos { get; set; }
         public RegistroCompraModel (AppDbContext context) {
             _context = context;
+
+            Productos = new List<Producto> {
+                new Producto {
+                Nombre = "Borradores",
+                IdImagenNavigation = new Imagen {
+                Nombre = "/Productos/borradores.jpg"
+                }
+
+                },
+                new Producto {
+                Nombre = "Colores",
+                IdImagenNavigation = new Imagen {
+                Nombre = "/Productos/colores.jpg"
+                }
+                },
+                new Producto {
+                Nombre = "Cuadernos",
+                IdImagenNavigation = new Imagen {
+                Nombre = "/Productos/cuaderno.jpg"
+                }
+                },
+                new Producto {
+                Nombre = "Lapiceros",
+                IdImagenNavigation = new Imagen {
+                Nombre = "/Productos/lapiceros.jpg"
+                }
+                },
+                new Producto {
+                Nombre = "Borradores",
+                IdImagenNavigation = new Imagen {
+                Nombre = "/Productos/borradores.jpg"
+                }
+
+                },
+                new Producto {
+                Nombre = "Colores",
+                IdImagenNavigation = new Imagen {
+                Nombre = "/Productos/colores.jpg"
+                }
+                }
+            };
         }
 
         [BindProperty]
@@ -29,5 +71,10 @@ namespace SGLibreria.Pages.Compras {
             await _context.SaveChangesAsync ();
             return RedirectToPage ("/Proveedores/RegistroProveedor");
         }
+
+        public void OnGet () {
+
+        }
+
     }
 }
--- Pages/Productos/ListaProducto.cshtml.cs
+++ Pages/Productos/ListaProducto.cshtml.cs
@@ -1,61 +1,58 @@
-
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SGLibreria.Models;
 
-namespace SGLibreria.Pages.Productos
-{
-    
-    public class ListaProductoModel: PageModel
-    {
-      public List<Producto> Productos {get;set;}
-      public ListaProductoModel() {
-                Productos = new List<Producto> {
-           new Producto{
-        Nombre = "Borradores", 
-        IdImagenNavigation = new Imagen{
-          Nombre = "/Productos/borradores.jpg"
+namespace SGLibreria.Pages.Productos {
+
+  public class ListaProductoModel : PageModel {
+    public List<Producto> Productos { get; set; }
+    public ListaProductoModel () {
+      Productos = new List<Producto> {
+        new Producto {
+        Nombre = "Borradores",
+        IdImagenNavigation = new Imagen {
+        Nombre = "/Productos/borradores.jpg"
         }
 
-        }, 
-        new Producto{
-        Nombre = "Colores", 
-        IdImagenNavigation = new Imagen{
-          Nombre = "/Productos/colores.jpg"
+        },
+        new Producto {
+        Nombre = "Colores",
+        IdImagenNavigation = new Imagen {
+        Nombre = "/Productos/colores.jpg"
         }
-        }, 
-        new Producto{
-        Nombre = "Cuadernos", 
-        IdImagenNavigation = new Imagen{
-          Nombre = "/Productos/cuaderno.jpg"
+        },
+        new Producto {
+        Nombre = "Cuadernos",
+        IdImagenNavigation = new Imagen {
+        Nombre = "/Productos/cuaderno.jpg"
         }
-        }, 
-        new Producto{
-        Nombre = "Lapiceros", 
-        IdImagenNavigation = new Imagen{
-          Nombre = "/Productos/lapiceros.jpg"
+        },
+        new Producto {
+        Nombre = "Lapiceros",
+        IdImagenNavigation = new Imagen {
+        Nombre = "/Productos/lapiceros.jpg"
         }
-        }, 
-        new Producto{
-        Nombre = "Borradores", 
-        IdImagenNavigation = new Imagen{
-          Nombre = "/Productos/borradores.jpg"
+        },
+        new Producto {
+        Nombre = "Borradores",
+        IdImagenNavigation = new Imagen {
+        Nombre = "/Productos/borradores.jpg"
         }
-        
-        }, 
-        new Producto{
-        Nombre = "Colores", 
-        IdImagenNavigation = new Imagen{
-          Nombre = "/Productos/colores.jpg"
-        }
-        }
-        };
 
-      }
-
-      public void OnGet() {
-      
-      }
+        },
+        new Producto {
+        Nombre = "Colores",
+        IdImagenNavigation = new Imagen {
+        Nombre = "/Productos/colores.jpg"
+        }
+        }
+      };
 
     }
+
+    public void OnGet () {
+
+    }
+
+  }
 }
