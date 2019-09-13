59c0c15700d0239cc1e3d2840d5f86f53f56a1fd
Diego Palacios <palaciosdiego759@gmail.com>
2019-09-13 03:23:45 +0200

    se cambio ventas y compras al principio del menu

---
+++ Pages/Ventas/ListaVenta.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SGLibreria.Pages.Ventas
+{
+    public class ListaVentaModel: PageModel
+    {
+        
+        public ListaVentaModel() {
+
+        }
+        public void OnGet() {
+
+        }
+    }
+}
