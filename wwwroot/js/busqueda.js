4856d7c20b7377d2d31b1c3a8304b2b1eb72a8ab
tepokev <tepokev@gmail.com>
2019-09-15 19:38:21 -0600

    Se corrigieron errores en el script de busqueda.js y se agrego un nuevo estilo en la bitacora

--- wwwroot/js/busqueda.js
+++ wwwroot/js/busqueda.js
@@ -1,39 +1,18 @@
-var doc = document;
-var elementdiv = doc.getElementsByClassName("search-box");
+var elementdiv = document.getElementsByClassName("search-box");
 for(var i = 0 ; i < elementdiv.length ; i++ )
 {
     elementdiv[i].addEventListener("mouseover", divhover);
-    nHover = 0;
     elementdiv[i].addEventListener("mouseout", divout);
-    nOut = 0;
 }
 
 function divhover(event){
-    var target = null;
-    if(event.currentTarget.className == "search-box"){
-    target = event.currentTarget;
-    }else{
-    return;
-    }
-    console.log("TargetHov:");
-    console.log(target);
+    var target = event.currentTarget;
     var inputs = target.getElementsByClassName("search-txt");
-    if(inputs.length > 0) {
     inputs[0].focus();
-    }
 }
 
 function divout(event){
-    var target = null;
-    if(event.currentTarget.className == "search-box"){
-    target = event.currentTarget;
-    }else{
-    return;
-    }
-    console.log("TargetOut:");
-    console.log(target);
+    var target = event.currentTarget;
     var inputs = target.getElementsByClassName("search-txt");
-    if(inputs.length > 0) {
     inputs[0].blur();
-    }
 }
