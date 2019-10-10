5c01c4087bbbcb058dcfaf1484e00fd7d9b3f595
Diego Palacios <palaciosdiego759@gmail.com>
2019-10-10 23:42:32 +0200

    utilidad para javascript, incluye ajax y algunas utilidades para los formularios

---
+++ wwwroot/js/sgl.js
@@ -0,0 +1,65 @@
+var sgl = {
+    get: function (url, callback, params) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                callback(this.responseText);
+            }
+        };
+        xhttp.open("GET", url, true);
+        xhttp.send();
+
+    },
+    post: function (url, callback, params) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                callback(this.responseText);
+            }
+        };
+        xhttp.open("POST", url, true);
+        xhttp.send();
+    },
+    ajax: function (metodo, url, callback, params) {
+        if (metodo.toUpperCase() == 'POST') {
+            cargarPorPost(url, callback);
+        } else if (metodo.toUpperCase() == 'GET') {
+            cargarPorGet(url, callback);
+        }
+    },
+    
+    clearForm: function (myFormElement) {
+        var elements = myFormElement.elements;
+        myFormElement.reset();
+        for (i = 0; i < elements.length; i++) {
+            field_type = elements[i].type.toLowerCase();
+            switch (field_type) {
+                case "text":
+                case "password":
+                case "textarea":
+                case "hidden":
+                    elements[i].value = "";
+                    break;
+                case "radio":
+                case "checkbox":
+                    if (elements[i].checked) {
+                        elements[i].checked = false;
+                    }
+                    break;
+                case "select-one":
+                case "select-multi":
+                    elements[i].selectedIndex = -1;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+};
+
+(function () {
+    sgl.ajax('get', 'formulario.html', function (data) {
+        var elem = document.getElementById('salida');
+        elem.innerHTML = data;
+    });
+})();
