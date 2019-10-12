76a485b8bb85ea4d8de27c2f92aaa4bccb541747
Diego Palacios <palaciosdiego759@gmail.com>
2019-10-12 21:49:16 +0200

    ListaProductoAjax

---
+++ Pages/Productos/ListaProductoAjax.cshtml.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SGLibreria.Models;
+
+namespace SGLibreria.Pages.Productos
+{
+    public class ListaProductoAjaxModel : PageModel
+    {
+        /*
+        Maximo es el número máximo de registros que mostrará está pagina
+        Pagina es el número de página o paginación que se va a mostrar
+        CantidadPorFila: número de productos que irán en cada <div class="card-group">
+        */
+        private readonly AppDbContext _context;
+        public List<Producto> Productos {get;set;}
+        
+        public int Pagina {get;set;}
+        public int CantidadPorFila {get;set;}
+        public int Maximo {get;set;}
+        public ListaProductoAjaxModel(AppDbContext context) {
+            this.Pagina = 0;
+            this.CantidadPorFila = 6;
+            this.Maximo = this.CantidadPorFila * 3;
+            _context = context;
+        }
+
+        public IActionResult OnGet(int? Pagina, int? CantidadPorFila, int? Maximo) {
+            this.Productos = _context.Productos
+            .Include(x=>x.IdImagenNavigation)
+            .Include(y=>y.Productoprecioventa)
+            .ToList();
+            return Page();
+        }
+        
+    }
+}
