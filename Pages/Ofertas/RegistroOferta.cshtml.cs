fbccd3c66d9c4c371e3511ced9b5d988e92e4d37
Student User <student@student-pc.student>
2019-11-03 23:53:54 -0600

    Se hicieron funcionar las View de Registro y Lista de Oferta

--- Pages/Ofertas/RegistroOferta.cshtml.cs
+++ Pages/Ofertas/RegistroOferta.cshtml.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SGLibreria.Models;
@@ -7,11 +8,28 @@ namespace SGLibreria.Pages.Ofertas {
         private readonly AppDbContext _context;
         [BindProperty]
         public Oferta Oferta {get; set;}
+        [BindProperty]
+        public int[] IdProductos { get; set; }
         public List<Producto> Productos { get; set; }
         public RegistroOfertaModel (AppDbContext context) {
             _context = context;
         }
         public void OnGet () {
+        }
+        public async Task<IActionResult> OnPostAsync(){
+            if(!ModelState.IsValid){
+                return NotFound();
+            }
+            await this._context.AddAsync(this.Oferta);
+            await this._context.SaveChangesAsync();
+            for(int i = 0 ; i < this.IdProductos.Length ; i ++){
+                var OfertaPrducto = new Ofertaproducto();
+                OfertaPrducto.IdProducto = this.IdProductos[i];
+                OfertaPrducto.IdOferta = Oferta.Id;
+                await this._context.AddAsync(OfertaPrducto);
+                await this._context.SaveChangesAsync();
+            }
+            return Page();
         }
     }
 }
