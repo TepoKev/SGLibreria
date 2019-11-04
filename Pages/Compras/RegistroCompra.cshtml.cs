7f682562ebd821ab2f391e23b345d26448540e71
Diego Palacios <palaciosdiego759@gmail.com>
2019-11-04 07:09:53 +0100

    Quien sabe que pasara con la Compra

--- Pages/Compras/RegistroCompra.cshtml.cs
+++ Pages/Compras/RegistroCompra.cshtml.cs
@@ -36,12 +36,16 @@ namespace SGLibreria.Pages.Compras {
             return Partial("_ProductoPartial",Producto);
         }
 */
-public IActionResult OnPost(){
+public async Task<JsonResult> OnPost(){
             //this.Producto.Estado = (sbyte)1;
             //_context.Productos.Add(Producto);
             //await _context.SaveChangesAsync();
             //return Partial("_ProductoPartial",Producto);
-            return Page();
+    return new JsonResult(
+        new {
+            Okay = "Okay fin"
+        }
+    );
 }
 
         public void OnGet () {
