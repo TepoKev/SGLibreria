aa5a00c30857e8b73a14667946d01289868f0da1
Ernesto <vacevedo242@gmail.com>
2019-11-19 22:37:26 -0600

    modificados manualmente la relacion de imagen en la tabla servicio y tiposervicio

--- Models/Servicio.cs
+++ Models/Servicio.cs
@@ -12,14 +12,8 @@ namespace SGLibreria.Models
         }
 
         public int Id { get; set; }
-        public int? IdImagen { get; set; }
-        [Required(ErrorMessage="El {0} es obligatorio"), StringLength(20, ErrorMessage="El campo {0} no puede contener mas de {1} caracteres")]
         public string Nombre { get; set; }
         public sbyte Estado { get; set; }
-        public int? IdCompania { get; set; }
-
-        public virtual Compania IdCompaniaNavigation { get; set; }
-        public virtual Imagen IdImagenNavigation { get; set; }
         public virtual ICollection<Tiposervicio> Tiposervicio { get; set; }
     }
 }
--- Models/Tiposervicio.cs
+++ Models/Tiposervicio.cs
@@ -17,9 +17,12 @@ namespace SGLibreria.Models
         [Required(ErrorMessage="El {0} es obligatorio")]
         public decimal Precio { get; set; }
         public int IdCompania { get; set; }
+        public int? IdImagen { get; set; }
+        [Required(ErrorMessage="El {0} es obligatorio"), StringLength(20, ErrorMessage="El campo {0} no puede contener mas de {1} caracteres")]
 
         public virtual Compania IdCompaniaNavigation { get; set; }
         public virtual Servicio IdServicioNavigation { get; set; }
+        public virtual Imagen IdImagenNavigation { get; set; }
         public virtual ICollection<Detalleservicio> Detalleservicio { get; set; }
     }
 }
