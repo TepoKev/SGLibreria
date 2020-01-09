7030a738d8e1f96c5d2d17293092cbd8d4aa74ce
ernesto <vacevedo242@gmail.com>
2020-01-09 13:19:08 -0600

    agregados cambios y agregado un nuevo package para intentar hacer el backup "dotnet add package Microsoft.SqlServer.SqlManagementObjects --version 150.18208.0"

---
+++ Models/BaseModelo.cs
@@ -0,0 +1,10 @@
+namespace SGLibreria.Models{
+
+    public class BaseModelo
+    {
+        public int PaginaActual {get;set;}
+        public int TotalDeRegistros {get;set;}
+        public int RegistrosPorPagina {get;set;}
+    }
+
+} 
