d92543cc0f675690625547c7775002cc374e40e5
TepoKev <tepokev@gmail.com>
2020-01-17 22:03:11 -0600

    Se agrego la consulta para el reporte de ventas

--- Pages/Ventas/ListaVenta.cshtml.cs
+++ Pages/Ventas/ListaVenta.cshtml.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -47,6 +49,80 @@ namespace SGLibreria.Pages.Ventas
                 item.IdTipoServicioNavigation.IdCompaniaNavigation = await this._context.Companias.Where(c => c.Id == item.IdTipoServicioNavigation.IdCompania).FirstOrDefaultAsync();
             }
             return Partial("_DetalleVentaPartial", this);
+        }
+
+        public async Task OnPostReporteVentaMes(DateTime mes){
+            IList<Venta> ventas = await this._context.Ventas
+            .Where(v => v.Fecha.Date.CompareTo(mes) == 0 )
+            .Include(v => v.Detalleventa).ThenInclude(dtv => dtv.IdPrecioVentaNavigation).ThenInclude(pv => pv.IdProductoNavigation).ThenInclude(p => p.Ofertaproducto).ThenInclude(ofp => ofp.IdOfertaNavigation)
+            .ToListAsync();
+            List<Conjunto> Conjuntos = new List<Conjunto>();
+            Conjunto Conjunto;
+            foreach(var venta in ventas){
+                foreach (var detalleventa in venta.Detalleventa)
+                {
+                    Conjunto = new Conjunto();
+                    Conjunto.Producto = detalleventa.IdPrecioVentaNavigation.IdProductoNavigation;
+                    Conjunto.Detalleventa = detalleventa;
+                    foreach (var ofertaproducto in detalleventa.IdPrecioVentaNavigation.IdProductoNavigation.Ofertaproducto)
+                    {
+                        if(ofertaproducto.IdOfertaNavigation.FechaInicio.CompareTo(mes) >= 0){
+                            Conjunto.Ofertaproducto = ofertaproducto;
+                        }
+                    }
+                    Conjuntos.Add(Conjunto);
+                }
+            }
+            List<ProductoT> productosT = new List<ProductoT>();
+            ProductoT productoT ;
+            foreach (var conjunto in Conjuntos)
+            {
+                if(productosT.Exists(p => p.Producto.Id == conjunto.Producto.Id)){
+                    var aux = productosT.Find(p => p.Producto.Id == conjunto.Producto.Id);
+                    productosT.Remove(aux);
+                    aux.Cantidad = aux.Cantidad;
+                    aux.PrecioUnitario = conjunto.Detalleventa.IdPrecioVentaNavigation.Valor;
+                    aux.Total = aux.Total + (aux.PrecioUnitario * aux.Cantidad);
+                    if(conjunto.Ofertaproducto != null){
+                        aux.TotalDesc = aux.TotalDesc + aux.PrecioUnitario * ((Decimal)conjunto.Ofertaproducto.IdOfertaNavigation.Descuento * (Decimal) 0.01);
+                    }
+                    productosT.Add(aux);
+                }else{
+                    productoT = new ProductoT();
+                    productoT.Producto = conjunto.Producto;
+                    productoT.Venta = conjunto.Detalleventa.IdVentaNavigation;
+                    productoT.Cantidad = conjunto.Detalleventa.Cantidad;
+                    productoT.PrecioUnitario = conjunto.Detalleventa.IdPrecioVentaNavigation.Valor;
+                    productoT.Total = productoT.Cantidad * productoT.PrecioUnitario;
+                    if(conjunto.Ofertaproducto != null){
+                        productoT.TotalDesc = productoT.PrecioUnitario * ((Decimal)conjunto.Ofertaproducto.IdOfertaNavigation.Descuento * (Decimal) 0.01);
+                    }
+                    productosT.Add(productoT);
+                }
+            }
+        }
+    }
+
+    public class Conjunto{
+        public Producto Producto { get; set; }
+        public Detalleventa Detalleventa { get; set; }
+        public Ofertaproducto Ofertaproducto { get; set; }
+        public Conjunto(){
+
+        }
+    }
+    public class ProductoT{
+        public Producto Producto { get; set; }
+        public Venta Venta { get; set; }
+        public int Cantidad { get; set; }
+        public Decimal PrecioUnitario { get; set; }
+        public Decimal Total { get; set; }
+        public Decimal TotalDesc { get; set; }
+        public ProductoT(){
+            this.Cantidad = 0;
+            this.PrecioUnitario = 0;
+            this.Total = 0;
+            this.TotalDesc = 0;
         }
     }
 }
