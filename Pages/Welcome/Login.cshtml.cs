0973ec3359c297f1a5bb189b8cbcd1ebf273254e
ernesto <vacevedo242@gmail.com>
2020-01-17 23:53:51 -0600

    corregido un error en el iniciar sesion

--- Pages/Welcome/Login.cshtml.cs
+++ Pages/Welcome/Login.cshtml.cs
@@ -33,57 +33,61 @@ namespace SGLibreria.Pages.Welcome
                 Session.Clear();
                 return Page();
             }
-
-            var user = await _context.Usuarios.AnyAsync(uc => uc.Correo == this.Usuario.Correo);
-            var clave = await _context.Usuarios.AnyAsync(uc => uc.Clave == Encrypted.Encrypt(this.Usuario.Clave));
-            
-            Usuario u = await _context.Usuarios
-            .Where(us =>
-               us.Nombre == Usuario.Correo
-               || us.Correo == Usuario.Correo
-            )
-            .Include(us => us.IdImagenNavigation)
-            .ThenInclude(Img => Img.IdRutaNavigation)
-            .Include(us => us.Empleado)
-            .ThenInclude(e => e.IdPersonaNavigation)
-            .SingleOrDefaultAsync();
-            if (u == null)
-            {
-                Mensaje = "Su usuario no existe";
+            if(Usuario.Correo == null || Usuario.Clave==null) {
+                Mensaje = "Complete los campos!";
                 return Page();
-            }
-            if (u != null && u.Estado == 0)
-            {
-                Mensaje = "¡Acceso denegado. Lo sentimos!";
-                return Page();
-            }
-            if(!clave){
-                Mensaje = "Contraseña incorrecta!.";
-                return Page();
-            }
-            if(u!=null && u.Estado == 1 && user && clave){
-                
-               
-                Persona per = u.Empleado.IdPersonaNavigation;
-                string NombreCompleto = per.NombreCompleto();
-                string ruta = "";
-                Imagen Imagen = u.IdImagenNavigation;
-                if (Imagen != null)
+            }else{
+                var user = await _context.Usuarios.AnyAsync(uc => uc.Correo == this.Usuario.Correo);
+                var clave = await _context.Usuarios.AnyAsync(uc => uc.Clave == Encrypted.Encrypt(this.Usuario.Clave));
+                Usuario u = await _context.Usuarios
+                .Where(us =>
+                us.Nombre == Usuario.Correo
+                || us.Correo == Usuario.Correo
+                )
+                .Include(us => us.IdImagenNavigation)
+                .ThenInclude(Img => Img.IdRutaNavigation)
+                .Include(us => us.Empleado)
+                .ThenInclude(e => e.IdPersonaNavigation)
+                .SingleOrDefaultAsync();
+                if (u == null)
                 {
-                    ruta = Imagen.IdRutaNavigation.Nombre + "/"
-                        + Imagen.Nombre;
+                    Mensaje = "Su usuario no existe";
+                    return Page();
                 }
-                Bitacora Bitacora = new Bitacora();
-                Bitacora.IdUsuario = u.Id;
-                Bitacora.InicioSesion = DateTime.Now;
-                this._context.Bitacoras.Add(Bitacora);
-                this._context.SaveChanges();
-                Session.SetInt32("IdUsuario", u.Id);
-                Session.SetInt32("IdBitacora", Bitacora.Id);
-                Session.SetInt32("Privilegio", u.Privilegio);
-                Session.SetString("NombreCompleto", NombreCompleto);
-                Session.SetString("Ruta", ruta);
+                if (u != null && u.Estado == 0)
+                {
+                    Mensaje = "¡Acceso denegado. Lo sentimos!";
+                    return Page();
+                }
+                if(!clave){
+                    Mensaje = "Contraseña incorrecta!.";
+                    return Page();
+                }
+                if(u!=null && u.Estado == 1 && user && clave){
+                    
+                
+                    Persona per = u.Empleado.IdPersonaNavigation;
+                    string NombreCompleto = per.NombreCompleto();
+                    string ruta = "";
+                    Imagen Imagen = u.IdImagenNavigation;
+                    if (Imagen != null)
+                    {
+                        ruta = Imagen.IdRutaNavigation.Nombre + "/"
+                            + Imagen.Nombre;
+                    }
+                    Bitacora Bitacora = new Bitacora();
+                    Bitacora.IdUsuario = u.Id;
+                    Bitacora.InicioSesion = DateTime.Now;
+                    this._context.Bitacoras.Add(Bitacora);
+                    this._context.SaveChanges();
+                    Session.SetInt32("IdUsuario", u.Id);
+                    Session.SetInt32("IdBitacora", Bitacora.Id);
+                    Session.SetInt32("Privilegio", u.Privilegio);
+                    Session.SetString("NombreCompleto", NombreCompleto);
+                    Session.SetString("Ruta", ruta);
+                }
             }
+            
             
             return RedirectToPage("/Index");
         }
