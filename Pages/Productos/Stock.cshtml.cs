d2d323dc1f7434c6fcf22f6a180bdab44cc88386
ernesto <vacevedo242@gmail.com>
2020-01-18 03:28:18 -0600

    ya funciona el aviso de stock minimo

--- Pages/Productos/Stock.cshtml.cs
+++ Pages/Productos/Stock.cshtml.cs
@@ -1,9 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using Microsoft.EntityFrameworkCore;
+using SGLibreria.Informes;
+using SGLibreria.Models;
 namespace SGLibreria.Pages.Productos
 {
     public class StockModel : PageModel
     {
+        
+        private readonly AppDbContext _context;
+        public IList<ConsultaProducto> ListProductos {get; set;}
 
+        public StockModel(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet(){
+            this.ListProductos = _context.ConsultaProducto.FromSql(ConsultaProducto.agotados()).ToList();
+            return Page();
+        }
     }
 }
